08fd308df37d9702aad631a637a736f0e3a8c705
Won Ying Keat <ykwon.2020@scis.smu.edu.sg>
2022-01-07 21:09:08 +0800

    add custom wordlist function

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ from nltk.corpus import words
 from nltk.metrics.distance import (
     edit_distance,
     jaccard_distance,
-    )
+)
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-download('words') #run this in the first run 
-correct_spellings = words.words() 
+download("words")  # run this in the first run
+correct_spellings = words.words()
 typos = []
 
 from pymongo import MongoClient
@@ -55,7 +55,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-ACCEPTABLE_SETTINGS = ["Spelling Hornets", "Profanity Alert"]
+ACCEPTABLE_SETTINGS = ["Spelling Hornets", "Profanity Alert", "Custom Wordlist"]
 
 reply_info_keyboard = [
     ["Spelling Hornets"],
@@ -64,7 +64,12 @@ reply_info_keyboard = [
     ["End"],
 ]
 
-reply_settings_keyboard = [["Spelling Hornets"], ["Profanity Alert"], ["End"]]
+reply_settings_keyboard = [
+    ["Spelling Hornets"],
+    ["Profanity Alert"],
+    ["Custom Wordlist"],
+    ["End"],
+]
 
 boolean_keyboard = [["On"], ["Off"], ["Cancel"]]
 
@@ -72,7 +77,7 @@ markup_info = ReplyKeyboardMarkup(reply_info_keyboard, one_time_keyboard=True)
 markup_settings = ReplyKeyboardMarkup(reply_settings_keyboard, one_time_keyboard=True)
 markup_boolean = ReplyKeyboardMarkup(boolean_keyboard, one_time_keyboard=True)
 
-START_STATE, BOOL_STATE = range(2)
+FIRST_STATE, SECOND_STATE, THIRD_STATE = range(3)
 
 load_dotenv("./.env")
 TOKEN = os.getenv("token")
@@ -87,7 +92,7 @@ db = client.annoyme
 PROFANITY_USERDB = db["profanity_user"]
 PROFANITY_TIMEDB = db["profanity_time"]
 SPELLING_ERRORDB = db["spelling"]
-SETTINGSDB = db["user_settings"]
+SETTINGSDB = db["chat_settings"]
 
 # https://pymongo.readthedocs.io/en/stable/tutorial.html
 # PROFANITY_USERDB.insert_one({"test": 123})
@@ -117,7 +122,7 @@ def message_check(update: Update, context: CallbackContext):
     profanity.load_censor_words()
 
     chat_id = update.message.chat.id
-    
+
     if profanity.contains_profanity(update.message.text) == True:
         user_id = str(update.effective_user.id)
         user_first_name = update.effective_user.first_name
@@ -126,7 +131,9 @@ def message_check(update: Update, context: CallbackContext):
         user_last_name = update.effective_user.last_name
         if user_last_name == None:
             user_last_name = " "
-        user_string = user_first_name + " (*&%^) " + user_last_name + " (*&%^) " + user_id
+        user_string = (
+            user_first_name + " (*&%^) " + user_last_name + " (*&%^) " + user_id
+        )
         datetime_now = datetime.datetime.now()
         if chat_id not in last_called:
             last_called[chat_id] = datetime_now
@@ -139,9 +146,27 @@ def message_check(update: Update, context: CallbackContext):
             userid_last_called = user_string_last_called.split(" (*&%^) ")[2]
             time_diff = str(datetime_now - datetime_last_called)
 
-            word_list = ['a mind\-boggling', 'an unbelievable', 'a spectacular', 'an exceptional', 'a mind\-blowing', 'an incredible', 'an inconceivable span of', 'an unimaginable', 'an impressive', 'a remarkable', 'a grand total of', 'a noteworthy', 'a shocking span of', 'a wondrous', 'a peaceful span of', 'a momentous', 'an astonishing']
+            word_list = [
+                "a mind\-boggling",
+                "an unbelievable",
+                "a spectacular",
+                "an exceptional",
+                "a mind\-blowing",
+                "an incredible",
+                "an inconceivable span of",
+                "an unimaginable",
+                "an impressive",
+                "a remarkable",
+                "a grand total of",
+                "a noteworthy",
+                "a shocking span of",
+                "a wondrous",
+                "a peaceful span of",
+                "a momentous",
+                "an astonishing",
+            ]
 
-            if 'day' in time_diff:
+            if "day" in time_diff:
                 break_list = time_diff.split(" day, ")
                 num_days = int(break_list[0])
                 num_hours = int((break_list[1]).split(":")[0])
@@ -150,9 +175,9 @@ def message_check(update: Update, context: CallbackContext):
 
                 word_input = str(total_hours) + " hours"
 
-            elif 'days' in time_diff:
-                word_input = (time_diff.split(", ")[0])
-            
+            elif "days" in time_diff:
+                word_input = time_diff.split(", ")[0]
+
             else:
                 break_list = time_diff.split(".")[0]
                 num_hours = int(break_list.split(":")[0])
@@ -168,36 +193,56 @@ def message_check(update: Update, context: CallbackContext):
 
             last_called[chat_id] = datetime_now
             last_called_username[chat_id] = user_string
-            rand_num = random.randint(0, len(word_list)-1)
-            update.message.reply_markdown_v2(fr"""🎉 *RESET THE COUNTER\!\!\!* 🎉
+            rand_num = random.randint(0, len(word_list) - 1)
+            update.message.reply_markdown_v2(
+                fr"""🎉 *RESET THE COUNTER\!\!\!* 🎉
             
 It has been _{word_list[rand_num]}_ *{word_input}* since someone spewed a vulgarity here\!
 
-Previous user to spew a vulgarity: [{firstname_last_called} {lastname_last_called}](tg://user?id={userid_last_called})""")
+Previous user to spew a vulgarity: [{firstname_last_called} {lastname_last_called}](tg://user?id={userid_last_called})"""
+            )
 
-    elif 'rick' in update.message.text.lower():
-        update.message.reply_video('https://c.tenor.com/x8v1oNUOmg4AAAAC/rickroll-roll.gif')
+    elif "rick" in update.message.text.lower():
+        update.message.reply_video(
+            "https://c.tenor.com/x8v1oNUOmg4AAAAC/rickroll-roll.gif"
+        )
 
     else:
-        count = 0 
+        count = 0
         for word in list_words:
             if word not in correct_spellings:
                 count += 1
-                candidates = [(jaccard_distance(set(ngrams(word, 2)), set(ngrams(w, 2))),w) for w in correct_spellings if w[0]==word[0]]
+                candidates = [
+                    (jaccard_distance(set(ngrams(word, 2)), set(ngrams(w, 2))), w)
+                    for w in correct_spellings
+                    if w[0] == word[0]
+                ]
                 if len(candidates) != 0:
-                    correction = sorted(candidates)[0] #gets most similar word based on jaccard distance
-                    typos.append((correction[0], word)) #adds jaccard score and original typo into list 
-        
-        context.chat_data["typos"] = typos #saves typos and scores into list 
-        
-        if count == 1: #if there are errors
-            update.message.reply_text(f"Your reply contained {str(count)} typo error! Are you even trying?")
+                    correction = sorted(candidates)[
+                        0
+                    ]  # gets most similar word based on jaccard distance
+                    typos.append(
+                        (correction[0], word)
+                    )  # adds jaccard score and original typo into list
+
+        context.chat_data["typos"] = typos  # saves typos and scores into list
+
+        if count == 1:  # if there are errors
+            update.message.reply_text(
+                f"Your reply contained {str(count)} typo error! Are you even trying?"
+            )
         if count > 1:
-            update.message.reply_text(f"Your reply contained {str(count)} typo errors! Are you even trying?")
-        if len(typos) > 10: #triggered when more than 10 errors, replies with worst jaccard score (i.e. 1)
-            typos.sort(key=lambda x:x[1])
+            update.message.reply_text(
+                f"Your reply contained {str(count)} typo errors! Are you even trying?"
+            )
+        if (
+            len(typos) > 10
+        ):  # triggered when more than 10 errors, replies with worst jaccard score (i.e. 1)
+            typos.sort(key=lambda x: x[1])
             worst_spelt = typos[-1][1]
-            update.message.reply_text(f"Someone made more than 10 typos today... Your worstly spelt word is {worst_spelt}")
+            update.message.reply_text(
+                f"Someone made more than 10 typos today... Your worstly spelt word is {worst_spelt}"
+            )
 
 
 def help(update: Update, context: CallbackContext):
@@ -213,7 +258,7 @@ def info(update: Update, context: CallbackContext):
     update.message.reply_markdown_v2(
         "Select one of these options:", reply_markup=markup_info
     )
-    return START_STATE
+    return FIRST_STATE
 
 
 def info_reply(update: Update, context: CallbackContext):
@@ -257,7 +302,7 @@ We also allow users to create memes of a pre\-defined template which links to th
 
     else:
         update.message.reply_text("I don't understand you...")
-    return START_STATE
+    return FIRST_STATE
 
 
 def settings(update: Update, context: CallbackContext):
@@ -265,7 +310,7 @@ def settings(update: Update, context: CallbackContext):
         "Choose which settings you would like to edit:", reply_markup=markup_settings
     )
 
-    return START_STATE
+    return FIRST_STATE
 
 
 def settings_reply(update: Update, context: CallbackContext):
@@ -278,17 +323,26 @@ def settings_reply(update: Update, context: CallbackContext):
             "chatid": update.message.chat_id,
             "Spelling Hornets": True,
             "Profanity Alert": True,
+            "wordlist": [],
         }
 
     context.chat_data["chat_settings"] = chat_settings
 
     if msg in ACCEPTABLE_SETTINGS:
+
+        if msg == "Custom Wordlist":
+            update.message.reply_text(
+                "Please enter words seperated by a comma. Example: word1, word2",
+                reply_markup=ReplyKeyboardRemove(),
+            )
+            return THIRD_STATE
+
         update.message.reply_text(
             f"Settings for {msg}: {'ON' if chat_settings[msg] else 'OFF'}.\nSelect the new settings:",
             reply_markup=markup_boolean,
         )
         context.chat_data["Settings_Option"] = msg
-        return BOOL_STATE
+        return SECOND_STATE
 
     if msg == "End":
         update.message.reply_text("Bye", reply_markup=ReplyKeyboardRemove())
@@ -297,7 +351,7 @@ def settings_reply(update: Update, context: CallbackContext):
     update.message.reply_text(
         f"Sorry, such settings does not exist", reply_markup=markup_settings
     )
-    return START_STATE
+    return FIRST_STATE
 
 
 def change_settings(update: Update, context: CallbackContext):
@@ -311,12 +365,12 @@ def change_settings(update: Update, context: CallbackContext):
         update.message.reply_text(
             "Sorry. This is not a valid option", reply_markup=markup_settings
         )
-        return_state = START_STATE
+        return_state = FIRST_STATE
     elif msg == "Cancel":
         update.message.reply_text(
             "Select the settings to edit:", reply_markup=markup_settings
         )
-        return_state = START_STATE
+        return_state = FIRST_STATE
     else:
         chat_settings[opt] = True if msg == "On" else False
         return_state = ConversationHandler.END
@@ -332,10 +386,37 @@ def change_settings(update: Update, context: CallbackContext):
     return return_state
 
 
+def change_wordlist(update: Update, context: CallbackContext):
+    msg = update.message.text
+    chat_settings = context.chat_data["chat_settings"]
+
+    try:
+        new_list = [a.strip() for a in msg.split(",")]
+        chat_settings["wordlist"] = new_list
+        update_obj = {"$set": chat_settings}
+
+        SETTINGSDB.update_one(
+            {"chatid": chat_settings["chatid"]},
+            update_obj,
+            upsert=True,
+        )
+
+        update.message.reply_text(
+            f"Wordlist successfully updated",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+    except:
+        update.message.reply_text(
+            f"The wordlist entered is malformed, please try again using /settings",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+    return ConversationHandler.END
+
+
 def meme_generator(update: Update, context: CallbackContext):
     update.message.reply_text("Please enter the text for the top line:")
 
-    return START_STATE
+    return FIRST_STATE
 
 
 def message_filter(update: Update, context: CallbackContext):
@@ -354,7 +435,7 @@ def message_filter(update: Update, context: CallbackContext):
     elif chat_id not in top_text:
         top_text[chat_id] = text
         update.message.reply_text("Please enter the text for the bottom line:")
-        return START_STATE
+        return FIRST_STATE
     else:
         bottom_text[chat_id] = text
 
@@ -403,7 +484,7 @@ def main():
     info_handler = ConversationHandler(
         entry_points=[CommandHandler("info", info)],
         states={
-            START_STATE: [MessageHandler(Filters.text, info_reply)],
+            FIRST_STATE: [MessageHandler(Filters.text, info_reply)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
@@ -411,8 +492,9 @@ def main():
     settings_handler = ConversationHandler(
         entry_points=[CommandHandler("settings", settings)],
         states={
-            START_STATE: [MessageHandler(Filters.text, settings_reply)],
-            BOOL_STATE: [MessageHandler(Filters.text, change_settings)],
+            FIRST_STATE: [MessageHandler(Filters.text, settings_reply)],
+            SECOND_STATE: [MessageHandler(Filters.text, change_settings)],
+            THIRD_STATE: [MessageHandler(Filters.text, change_wordlist)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
@@ -420,7 +502,7 @@ def main():
     meme_conv_handler = ConversationHandler(
         entry_points=[CommandHandler("memes", meme_generator)],
         states={
-            1: [MessageHandler(Filters.text, message_filter)],
+            FIRST_STATE: [MessageHandler(Filters.text, message_filter)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
         allow_reentry=True,
